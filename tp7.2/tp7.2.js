94e5cd821f722feced27a5e080d1c6bb1a8d7915
Albantoin <144117797+Albantoin@users.noreply.github.com>
2023-10-17 12:59:57 +0200

    Add files via upload

---
+++ tp7.2/tp7.2.js
@@ -0,0 +1,54 @@
+function somme(){
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+    var c = Number(a)+Number(b)
+
+    t3.value = c;
+}
+
+function soustraction() {
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+    var c = Number(a)-Number(b)
+
+    t3.value = c;
+}
+
+function multiplication() {
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+    var c = Number(a)*Number(b)
+
+    t3.value = c;
+}
+
+function division() {
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+    var c = Number(a)/Number(b)
+
+    t3.value = c;
+}
+
+function pariter() {
+    var v;
+    v = Number(document.getElementById("t3").value);
+    if (v%2==0) {
+    t4.value = ("pair")
+    }
+    else{   
+    t4.value = ("impair")
+ }
+}
+
+function permuter() {
+    var v3 = "";
+    a = document.getElementById("t1").value;
+    b = document.getElementById("t2").value;
+    v3 = a;
+    a = b;
+    b = v3;
+
+    t1.value = a;
+    t2.value = b;
+}
